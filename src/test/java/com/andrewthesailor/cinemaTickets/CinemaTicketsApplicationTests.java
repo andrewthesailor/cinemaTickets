3551f9298429fdf28716e76497f65130fcf753e9
ats-wololo <andrew.szczepanik@outlook.com>
2020-02-13 19:53:51 +0100

    initial files from spring initializr

---
+++ src/test/java/com/andrewthesailor/cinemaTickets/CinemaTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewthesailor.cinemaTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
